9e0a5fd69539f98e92c56af9f529442676b82e4e
tgarrel1 <tgarrel1@uncc.edu>
2015-11-05 20:02:29 +0000

    Adding Movies Feature Done

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -22,6 +22,21 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
+  When I check the following ratings: PG, R
+  And I uncheck the following ratings: PG-13, G
+  And I press "Refresh"
+
+  Then I should see "The Terminator"
+  And I should see "When Harry Met Sally"
+  And I should see "Amelie"
+  And I should see "The Incredibles"
+  And I should see "Raiders of the Lost Ark"
+
+  And I should not see "Aladdin"
+  And I should not see "The Help"
+  And I should not see "Chocolat"
+  And I should not see "2001: A Space Odyssey"
+  And I should not see "Chicken Run"
   # enter step(s) to check the 'PG' and 'R' checkboxes
   # enter step(s) to uncheck all other checkboxes
   # enter step to "submit" the search form on the homepage
@@ -29,4 +44,6 @@ Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to ensure that other movies are not visible
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: G, PG, PG-13, R
+  
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,16 +2,18 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  assert page.body =~ /#{e1}.*#{e2}/m
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   fail "Unimplemented"
@@ -22,10 +24,18 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(", ").each do |rating|
+    if uncheck == False
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
+  click_button("Refresh")
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
